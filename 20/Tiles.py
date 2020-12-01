97d08d56c0de90a06c5b10618c0d636aa6ba1956
simonsandell <simonsandell@live.se>
2020-12-01 20:20:14 +0100

    something?

--- 20/Tiles.py
+++ 20/Tiles.py
@@ -21,8 +21,14 @@ class Tiles:
     def __init__(self):
         self.panels = {}
 
+        self.max_x = 0
+        self.max_y = 0
+
         self.portals = {}
         self.extra_adjacent = {}
+
+        self.inner_portals = set()
+        self.outer_portals = set()
 
         self.startpos = None
         self.endpos = None
@@ -30,8 +36,14 @@ class Tiles:
         self.visited = set()
         self.steps = 0
 
+        self.adjacent_portal_cache = {}
+
     def set_color(self, pos, color):
         self.panels[pos] = color
+        if pos[0] > self.max_x:
+            self.max_x = pos[0]
+        if pos[1] > self.max_y:
+            self.max_y = pos[1]
 
     def get_color(self, pos):
         if pos in self.panels:
@@ -43,6 +55,17 @@ class Tiles:
             if self.get_color(pos) == '.':
                 return pos
         raise Exception('No hallway found')
+
+    def is_outer(self, pos):
+        if pos[0] < 4:
+            return True
+        if pos[0] > (self.max_x - 5):
+            return True
+        if pos[1] < 4:
+            return True
+        if pos[1] > (self.max_y - 5):
+            return True
+        return False
 
     def find_portals(self):
         for pos, char in self.panels.items():
@@ -73,6 +96,14 @@ class Tiles:
 
                 self.extra_adjacent[a] = b
                 self.extra_adjacent[b] = a
+
+                if self.is_outer(a):
+                    self.outer_portals.add(a)
+                    self.inner_portals.add(b)
+                else:
+                    self.outer_portals.add(b)
+                    self.inner_portals.add(a)
+
             if portal == 'AA':
                 adj = Tiles.get_adjacent_positions(spanning[0])
                 adj.extend(Tiles.get_adjacent_positions(spanning[1]))
@@ -120,16 +151,93 @@ class Tiles:
         out += 'Steps: '  + str(self.steps) + '\n'
         return out
 
-    def get_adjacent_hallways(self, pos):
+    def get_adjacent_hallways(self, pos, use_portals=True):
         r = set()
-        if pos in self.extra_adjacent:
-            r.add(self.extra_adjacent[pos])
+        if use_portals:
+            if pos in self.extra_adjacent:
+                r.add(self.extra_adjacent[pos])
 
         adj = Tiles.get_adjacent_positions(pos)
         for a in adj:
             if self.get_color(a) == '.':
                 r.add(a)
         return r
+
+    def build_cache(self):
+        portals = self.outer_portals.copy()
+        portals = portals.union(self.inner_portals.copy())
+        portals.add(self.startpos)
+        for startpos in portals:
+            self.adjacent_portal_cache[startpos] = set()
+            steps = 0
+            visited = set()
+            visited.add(startpos)
+            edges = self.get_adjacent_hallways(startpos, False)
+            while True:
+                steps += 1
+                next_edges = set()
+                for e in edges:
+                    visited.add(e)
+                    if e in self.outer_portals:
+                        self.adjacent_portal_cache[startpos].add((e, steps+1, -1))
+                    if e in self.inner_portals:
+                        self.adjacent_portal_cache[startpos].add((e, steps+1, 1))
+                    if e == self.endpos:
+                        self.adjacent_portal_cache[startpos].add((e, steps+1, 0))
+                    pne = self.get_adjacent_hallways(e, False)
+                    for pe in pne:
+                        if pe not in visited:
+                            next_edges.add(pe)
+                edges = next_edges
+                if len(edges) == 0:
+                    break
+
+    def query_cache(self, portal, taken_steps, level):
+        r = []
+        adj = self.adjacent_portal_cache[portal]
+        for pos, steps, levelshift in adj:
+            if pos == self.endpos and level == 0:
+                return [(pos, taken_steps + steps, 0)]
+            next_level = level + levelshift
+            if next_level > 0:
+                if pos in self.extra_adjacent:
+                    r.append((self.extra_adjacent[pos], steps + taken_steps, next_level))
+                elif pos != self.endpos:
+                    raise Exception(pos, level)
+        return r
+
+    def walk_recursively(self):
+        steps = 0
+        startpos = self.startpos
+        startlevel = 0
+
+        finish = 99999999
+        visited = set()
+        visited.add((startpos, startlevel))
+        states = [(startpos, steps, startlevel)]
+
+        while len(states) > 0:
+            print(states)
+            input()
+            current_state = states[0]
+            states.remove(current_state)
+            adj = self.query_cache(current_state[0], current_state[1], current_state[2])
+            for a in adj:
+                if a[0] == self.endpos:
+                    if a[1] < finish:
+                        finish = a[1]
+                if (a[0], a[2]) not in visited:
+                    states.append(a)
+                    visited.add((a[0], a[2]))
+            all_greater = True
+            for s in states:
+                if s[1] < finish:
+                    all_greater = False
+            if all_greater:
+                return finish
+            self.visited.add(current_state[0])
+            self.steps = current_state[2]
+            print(self)
 
     def walk(self, fr, to):
         steps = {}
